785c78169a09195dbcb5edd8a909d75c672ed7df
junhyok7146 <junhyeok_an@naver.com>
2024-04-24 10:00:25 +0900

    안준혁 수정

--- src/components/actor/ActorSection.jsx
+++ src/components/actor/ActorSection.jsx
@@ -30,8 +30,9 @@ const ActorSection = ({searchWorks}) => {
                         <li key={index}>
                             <a href="#">
                                 <img src={item.photo} alt={item.title} />
-                                <span>작품명 : {item.title}</span>
-                                <span>출연배우 : {item.name}</span>
+                                <span>안준혁 작품명 : {item.title}</span>
+                                <span>안준혁 출연배우 : {item.name}</span>
+                                
                             </a>
                         </li>
                     ))
