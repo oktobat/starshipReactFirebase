e3e845963bac8ad54b8a817810afcff90d309fa4
jinsol <emcis44@gmail.com>
2024-04-24 10:29:52 +0900

    백진솔 수정

--- src/components/actor/ActorSearch.jsx
+++ src/components/actor/ActorSearch.jsx
@@ -1,45 +1,51 @@
-import React, {useState} from 'react';
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const ActorSearchBlock = styled.div`
-    text-align: center;
-    margin-bottom: 70px;
-    input { border: 1px solid #e8ecf2;
-        font-size: 16px;
-        padding: 10px 20px;
-        border-radius: 50px;
-        max-width: 300px;
-        margin-right: 10px;
-    }
-    button { color: #fff;
-        background: #00c183;
-        border-radius: 50px;
-        padding: 10px 50px;
-    }
-`
+  text-align: center;
+  margin-bottom: 70px;
+  input {
+    border: 1px solid #e8ecf2;
+    font-size: 16px;
+    padding: 10px 20px;
+    border-radius: 50px;
+    max-width: 300px;
+    margin-right: 10px;
+  }
+  button {
+    color: #fff;
+    background: #00c183;
+    border-radius: 50px;
+    padding: 10px 50px;
+  }
+`;
 
-const ActorSearch = ({onSearch}) => {
+const ActorSearch = ({ onSearch }) => {
+  const [text, setText] = useState("");
 
-    const [text, setText] = useState("")
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onSearch(text, "search");
+    setText("");
+  };
 
-    const onSubmit = (e)=>{
-        e.preventDefault()
-        onSearch(text, "search")
-        setText("")
-    }
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
 
-    const onChange = (e)=>{
-        setText(e.target.value)
-    }
-
-    return (
-        <ActorSearchBlock>
-            <form onSubmit={onSubmit}>
-                <input type="text" placeholder="검색어를 입력하세요." value={text} onChange={onChange} />
-                <button type="submit">검색하기</button>
-            </form>
-        </ActorSearchBlock>
-    );
+  return (
+    <ActorSearchBlock>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="백진솔 검색어를 입력하세요."
+          value={text}
+          onChange={onChange}
+        />
+        <button type="submit">검색하기</button>
+      </form>
+    </ActorSearchBlock>
+  );
 };
 
 export default ActorSearch;
